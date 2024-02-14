9645aa5e78f7d64f2db278114f746869c2c91a17
rohankumar2025 <rohankumar@ucsb.edu>
2024-02-13 17:42:01 -0800

    added backend files from team02

---
+++ src/main/java/edu/ucsb/cs156/example/entities/UCSBOrganization.java
@@ -0,0 +1,26 @@
+package edu.ucsb.cs156.example.entities;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity(name = "ucsborganizations")
+public class UCSBOrganization {
+  @Id
+  private String orgCode;
+  private String orgTranslationShort;
+  private String orgTranslation;  
+  private boolean inactive;
+}
