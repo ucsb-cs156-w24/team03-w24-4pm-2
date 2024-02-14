b2a36760f1795844230e9cc3ddeca94324b5a8ff
Kyle Kumar <kylebkumar@gmail.com>
2024-02-14 15:34:06 -0800

    Added fixtures for UCSBDiningCommonsMenuItem

---
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
@@ -0,0 +1,30 @@
+const ucsbDiningCommonsMenuItemFixtures = {
+    oneDiningCommonsMenuItem: {
+        "id": 1,
+        "diningCommonsCode": "portola",
+        "name": "burger",
+        "station": "grill"
+      },
+    threeDiningCommonsMenuItems: [
+        {
+            "id": 1,
+            "diningCommonsCode": "portola",
+            "name": "burger",
+            "station": "grill"
+        },
+        {
+            "id": 2,
+            "diningCommonsCode": "de la guerra",
+            "name": "pizza",
+            "station": "bakery"
+          },
+          {
+            "id": 3,
+            "diningCommonsCode": "carillo",
+            "name": "braised lamb ragu",
+            "station": "euro"
+        }]
+}
+
+
+export { ucsbDiningCommonsMenuItemFixtures };
