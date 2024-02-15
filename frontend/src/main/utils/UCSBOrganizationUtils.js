4d4dd4fe2efe0e1b636615d3ce27870a4541bfac
rohankumar2025 <rohankumar@ucsb.edu>
2024-02-15 13:37:37 -0800

    added tests for util

--- frontend/src/main/utils/UCSBOrganizationUtils.js
+++ frontend/src/main/utils/UCSBOrganizationUtils.js
@@ -10,7 +10,7 @@ export function cellToAxiosParamsDelete(cell) {
         url: "/api/ucsborganizations",
         method: "DELETE",
         params: {
-            id: cell.row.values.id
+            orgCode: cell.row.values.orgCode
         }
     }
 }
