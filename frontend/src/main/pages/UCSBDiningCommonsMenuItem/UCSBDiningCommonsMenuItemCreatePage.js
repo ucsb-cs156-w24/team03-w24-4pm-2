5cbb3a3e3c45f1df88dba0fe692c54bcef0a3c0f
Kyle Kumar <kylebkumar@gmail.com>
2024-02-14 21:03:41 -0800

    Implemented Create Menu Item page

--- frontend/src/main/pages/UCSBDiningCommonsMenuItem/UCSBDiningCommonsMenuItemCreatePage.js
+++ frontend/src/main/pages/UCSBDiningCommonsMenuItem/UCSBDiningCommonsMenuItemCreatePage.js
@@ -1,29 +1,30 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
-import RestaurantForm from "main/components/Restaurants/RestaurantForm";
+import UCSBDiningCommonsMenuItemForm from "main/components/UCSBDiningCommonsMenuItem/UCSBDiningCommonsMenuItemForm";
 import { Navigate } from 'react-router-dom'
 import { useBackendMutation } from "main/utils/useBackend";
 import { toast } from "react-toastify";
 
 export default function UCSBDiningCommonsMenuItemCreatePage({storybook=false}) {
 
-  const objectToAxiosParams = (restaurant) => ({
-    url: "/api/restaurants/post",
+  const objectToAxiosParams = (diningcommonsmenuitem) => ({
+    url: "/api/ucsbdiningcommonsmenuitem/post",
     method: "POST",
     params: {
-     name: restaurant.name,
-     description: restaurant.description
+     name: diningcommonsmenuitem.name,
+     diningCommonsCode: diningcommonsmenuitem.diningCommonsCode,
+     station: diningcommonsmenuitem.station,
     }
   });
 
-  const onSuccess = (restaurant) => {
-    toast(`New restaurant Created - id: ${restaurant.id} name: ${restaurant.name}`);
+  const onSuccess = (diningcommonsmenuitem) => {
+    toast(`New dining commons menu item Created - id: ${diningcommonsmenuitem.id} name: ${diningcommonsmenuitem.name}`);
   }
 
   const mutation = useBackendMutation(
     objectToAxiosParams,
      { onSuccess }, 
      // Stryker disable next-line all : hard to set up test for caching
-     ["/api/restaurants/all"] // mutation makes this key stale so that pages relying on it reload
+     ["/api/diningcommonsmenuitem/all"] // mutation makes this key stale so that pages relying on it reload
      );
 
   const { isSuccess } = mutation
@@ -33,14 +34,14 @@ export default function UCSBDiningCommonsMenuItemCreatePage({storybook=false}) {
   }
 
   if (isSuccess && !storybook) {
-    return <Navigate to="/restaurants" />
+    return <Navigate to="/diningcommonsmenuitem" />
   }
 
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>Create New Restaurant</h1>
-        <RestaurantForm submitAction={onSubmit} />
+        <h1>Create New Menu Item</h1>
+        <UCSBDiningCommonsMenuItemForm submitAction={onSubmit} />
       </div>
     </BasicLayout>
   )
