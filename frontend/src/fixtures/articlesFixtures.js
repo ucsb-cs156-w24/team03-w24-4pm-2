31a9205cef3891bc6650db75fbe64c2aa9647c31
Bharat Kathi <bharat1031@gmail.com>
2024-02-15 17:28:09 -0800

    add article fixtures

---
+++ frontend/src/fixtures/articlesFixtures.js
@@ -0,0 +1,40 @@
+const articleFixtures = {
+    oneArticle: [
+        {
+            "id": 1,
+            "title": "Handy Spring Utility Classes",
+            "url": "https://twitter.com/maciejwalkowiak/status/1511736828369719300?t=gGXpmBH4y4eY9OBSUInZEg&s=09",
+            "explanation": "A lot of really useful classes are built into Spring",
+            "email": "bkathi@ucsb.edu",
+            "dateAdded": "2024-02-09T12:00:00"
+        }
+    ],
+    threeArticles: [
+        {
+            "id": 2,
+            "title": "Article 2",
+            "url": "https://example.com/article2",
+            "explanation": "Explanation for Article 2",
+            "email": "example@example.com",
+            "dateAdded": "2024-02-09T12:00:00"
+        },
+        {
+            "id": 3,
+            "title": "Article 3",
+            "url": "https://example.com/article3",
+            "explanation": "Explanation for Article 3",
+            "email": "example@example.com",
+            "dateAdded": "2024-02-09T12:00:00"
+        },
+        {
+            "id": 4,
+            "title": "Article 4",
+            "url": "https://example.com/article4",
+            "explanation": "Explanation for Article 4",
+            "email": "example@example.com",
+            "dateAdded": "2024-02-09T12:00:00"
+        }
+    ]
+};
+
+export { articleFixtures };
