b99e239af578adcde24420973ee8b3fedba8cd26
Bharat Kathi <bharat1031@gmail.com>
2024-02-15 17:58:12 -0800

    fix tests

--- frontend/src/fixtures/articlesFixtures.js
+++ frontend/src/fixtures/articlesFixtures.js
@@ -1,40 +1,39 @@
 const articlesFixtures = {
-    oneArticle: [
+    oneArticle: {
+        "id": 1,
+        "title": "Test",
+        "url": "test.com",
+        "explanation": "test article",
+        "email": "test@gmail.com",
+        "dateAdded": "2022-01-02T12:00:00"
+    },
+    threeArticle: [
         {
             "id": 1,
-            "title": "Handy Spring Utility Classes",
-            "url": "https://twitter.com/maciejwalkowiak/status/1511736828369719300?t=gGXpmBH4y4eY9OBSUInZEg&s=09",
-            "explanation": "A lot of really useful classes are built into Spring",
-            "email": "bkathi@ucsb.edu",
-            "dateAdded": "2024-02-09T12:00:00"
-        }
-    ],
-    threeArticles: [
+            "title": "Test1",
+            "url": "test1.com",
+            "explanation": "test article 1",
+            "email": "test1@gmail.com",
+            "dateAdded": "2022-01-02T12:00:00"
+        },
         {
             "id": 2,
-            "title": "Article 2",
-            "url": "https://example.com/article2",
-            "explanation": "Explanation for Article 2",
-            "email": "example@example.com",
-            "dateAdded": "2024-02-09T12:00:00"
+            "title": "Test2",
+            "url": "test2.com",
+            "explanation": "test article 2",
+            "email": "test2@gmail.com",
+            "dateAdded": "2022-04-03T12:00:00"
         },
         {
             "id": 3,
-            "title": "Article 3",
-            "url": "https://example.com/article3",
-            "explanation": "Explanation for Article 3",
-            "email": "example@example.com",
-            "dateAdded": "2024-02-09T12:00:00"
-        },
-        {
-            "id": 4,
-            "title": "Article 4",
-            "url": "https://example.com/article4",
-            "explanation": "Explanation for Article 4",
-            "email": "example@example.com",
-            "dateAdded": "2024-02-09T12:00:00"
+            "title": "Test3",
+            "url": "test3.com",
+            "explanation": "test article 3",
+            "email": "test3@gmail.com",
+            "dateAdded": "2022-07-04T12:00:00"
         }
     ]
 };
+
 
 export { articlesFixtures };
