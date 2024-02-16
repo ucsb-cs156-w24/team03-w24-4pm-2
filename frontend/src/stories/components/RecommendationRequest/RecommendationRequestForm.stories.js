00d786b219f7d9455d61925c8349b7b5776540fd
Alex Lim <alex@virtueware.com>
2024-02-16 03:55:17 -0800

    fixing issue with story

--- frontend/src/stories/components/RecommendationRequest/RecommendationRequestForm.stories.js
+++ frontend/src/stories/components/RecommendationRequest/RecommendationRequestForm.stories.js
@@ -27,7 +27,7 @@ Create.args = {
 export const Update = Template.bind({});
 
 Update.args = {
-    initialContents: recommendationRequestFixtures.oneDate,
+    initialContents: recommendationRequestFixtures.oneRecommendationRequest,
     buttonLabel: "Update",
     submitAction: (data) => {
         console.log("Submit was clicked with data: ", data); 
