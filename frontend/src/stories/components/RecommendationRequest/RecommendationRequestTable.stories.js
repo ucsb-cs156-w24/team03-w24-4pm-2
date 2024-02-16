c58a830530de754c9f0c32d33b5d87ecd0282e7d
Alex Lim <alex@virtueware.com>
2024-02-16 03:48:48 -0800

    fixing issue with stories

--- frontend/src/stories/components/RecommendationRequest/RecommendationRequestTable.stories.js
+++ frontend/src/stories/components/RecommendationRequest/RecommendationRequestTable.stories.js
@@ -18,19 +18,19 @@ const Template = (args) => {
 export const Empty = Template.bind({});
 
 Empty.args = {
-    dates: []
+    recommendationRequests: []
 };
 
 export const ThreeItemsOrdinaryUser = Template.bind({});
 
 ThreeItemsOrdinaryUser.args = {
-    dates: recommendationRequestFixtures.threeDates,
+    recommendationRequests: recommendationRequestFixtures.threeRecommendationRequests,
     currentUser: currentUserFixtures.userOnly,
 };
 
 export const ThreeItemsAdminUser = Template.bind({});
 ThreeItemsAdminUser.args = {
-    dates: recommendationRequestFixtures.threeDates,
+    recommendationRequests: recommendationRequestFixtures.threeRecommendationRequests,
     currentUser: currentUserFixtures.adminUser,
 }
 
