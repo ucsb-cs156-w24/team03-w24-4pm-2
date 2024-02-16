8886c9acbdbc6c582daefce10ef7b50e9c8e0784
Alex Lim <alex@virtueware.com>
2024-02-16 03:40:06 -0800

    fixing utils

--- frontend/src/main/utils/RecommendationRequestUtils.js
+++ frontend/src/main/utils/RecommendationRequestUtils.js
@@ -7,7 +7,7 @@ export function onDeleteSuccess(message) {
 
 export function cellToAxiosParamsDelete(cell) {
     return {
-        url: "/api/ucsbdates",
+        url: "/api/recommendationrequest",
         method: "DELETE",
         params: {
             id: cell.row.values.id
