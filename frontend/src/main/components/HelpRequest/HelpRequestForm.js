6d3d34add575312dcc889a20a7372a4a62875bba
Kyle Kumar <kylebkumar@gmail.com>
2024-02-20 21:19:55 -0800

    Fix lint

--- frontend/src/main/components/HelpRequest/HelpRequestForm.js
+++ frontend/src/main/components/HelpRequest/HelpRequestForm.js
@@ -16,8 +16,6 @@ function HelpRequestForm({ initialContents, submitAction, buttonLabel = "Create"
 
     const navigate = useNavigate();
 
-    const testIdPrefix = "HelpRequestForm";
-
     // For explanation, see: https://stackoverflow.com/questions/3143070/javascript-regex-iso-datetime
     // Note that even this complex regex may still need some tweaks
 
